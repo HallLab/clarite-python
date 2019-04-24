46757450d2f4a01ad37c83f916f24ae8cbc28d98
John McGuigan <jrm5100@psu.edu>
2019-04-24 15:03:06 -0400

    Add first colfilter and first rowfilter using Pandas accessors

--- clarite/__init__.py
+++ clarite/__init__.py
@@ -1,4 +1,8 @@
-
+# flake8: noqa
 from ._version import get_versions
+
+from .colfilters import ColFilterAccessor
+from .rowfilters import RowFilterAccessor
+
 __version__ = get_versions()['version']
 del get_versions
