1b1d00f84701f8675eeb595456daa7aff49a8679
John McGuigan <jrm5100@psu.edu>
2019-04-24 16:27:34 -0400

    Add more filtering functions

--- clarite/__init__.py
+++ clarite/__init__.py
@@ -3,6 +3,7 @@ from ._version import get_versions
 
 from .colfilters import ColFilterAccessor
 from .rowfilters import RowFilterAccessor
+from .io import load_data
 
 __version__ = get_versions()['version']
 del get_versions
--- clarite/colfilters.py
+++ clarite/colfilters.py
@@ -18,7 +18,7 @@ class ColFilterAccessor(object):
             raise AttributeError("")
 
     def percent_zero(self, max_proportion: float = 0.9, skip: Optional[List[str]] = None, only: Optional[List[str]] = None):
-        """Remove columns which have >= <max_proportion> values of zero (excluding NA)"""
+        """Remove columns which have <max_proportion> or more values of zero (excluding NA)"""
         df = self._obj
         columns = _validate_skip_only(list(df), skip, only)
         num_before = len(df.columns)
@@ -29,4 +29,30 @@ class ColFilterAccessor(object):
 
         print(f"Removed {num_removed:,} of {num_before:,} variables ({num_removed/num_before:.2%}) "
               f"which were equal to zero in at least {max_proportion:.2%} of non-NA observations.")
+        return df.loc[:, kept]
+
+    def min_n(self, n: int = 200, skip: Optional[List[str]] = None, only: Optional[List[str]] = None):
+        """Remove columns which have less than <n> values (excluding NA)"""
+        df = self._obj
+        columns = _validate_skip_only(list(df), skip, only)
+        num_before = len(df.columns)
+
+        counts = df.count()  # by default axis=0 (rows) so counts number of non-NA rows in each column
+        kept = (counts >= n) | ~df.columns.isin(columns)
+        num_removed = num_before - sum(kept)
+
+        print(f"Removed {num_removed:,} of {num_before:,} variables ({num_removed/num_before:.2%}) which had less than {n} values")
+        return df.loc[:, kept]
+
+    def min_cat_n(self, n: int = 200, skip: Optional[List[str]] = None, only: Optional[List[str]] = None):
+        """Remove columns which have less than <n> observations in each category"""
+        df = self._obj
+        columns = _validate_skip_only(list(df), skip, only)
+        num_before = len(df.columns)
+
+        min_category_counts = df.apply(lambda col: col.value_counts().min())
+        kept = (min_category_counts >= n) | ~df.columns.isin(columns)
+        num_removed = num_before - sum(kept)
+
+        print(f"Removed {num_removed:,} of {num_before:,} variables ({num_removed/num_before:.2%}) which had a category with less than {n} values")
         return df.loc[:, kept]
