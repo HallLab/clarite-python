9e52c1991e0abeaab8f13f258728e7896c519396
John McGuigan <jrm5100@psu.edu>
2019-10-01 11:48:57 -0400

    Hopefully really fixed this time, and limit the coverage report

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -110,6 +110,11 @@ html_theme = 'sphinx_rtd_theme'
 import sphinx_rtd_theme
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
+
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
@@ -118,11 +123,6 @@ html_logo = 'clarite_logo.png'
 html_theme_options = {
     'logo_only': True,
 }
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
