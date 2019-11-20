2f3f58f7e1f916faf58a6c1807b13d0ea9a63008
John McGuigan <jrm5100@psu.edu>
2019-11-20 13:19:42 -0500

    Initial attempt

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'README.rst'), encoding='utf-8') as readme_file:
     readme = readme_file.read()
 
-with open(path.join(here, 'requirements.txt')) as requirements_file:
+with open(path.join(here, 'requirements/requirements.txt')) as requirements_file:
     # Parse requirements.txt, ignoring any commented-out lines.
     requirements = [line for line in requirements_file.read().splitlines()
                     if not line.startswith('#')]
