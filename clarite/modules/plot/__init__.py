f798b5abb0939bce569210ae280b0c0a8b450e31
John McGuigan <jrm5100@psu.edu>
2020-10-23 12:33:17 -0400

    Split up plot functions into separate files

---
+++ clarite/modules/plot/__init__.py
@@ -0,0 +1,31 @@
+"""
+Plot
+========
+
+Functions that generate plots
+
+  .. autosummary::
+     :toctree: modules/plot
+
+     histogram
+     distributions
+     manhattan
+     manhattan_fdr
+     manhattan_bonferroni
+     top_results
+
+"""
+
+from .distributions import distributions
+from .histogram import histogram
+from .manhattan import manhattan, manhattan_fdr, manhattan_bonferroni
+from .top_results import top_results
+
+__all__ = [
+    distributions,
+    histogram,
+    manhattan,
+    manhattan_fdr,
+    manhattan_bonferroni,
+    top_results,
+]
