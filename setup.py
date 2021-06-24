2c05734b3ca327b5d554a9828039bfd5eb3f940a
John McGuigan <jrm5100@gmail.com>
2021-06-24 00:04:02 -0400

    Revert "Switch to Poetry, and update test data with some rounded values to try and help comparisons (#102)"
    This reverts commit 33aaaa3e65fedb84ef226ce0875907f242231f7f.

---
+++ setup.py
@@ -0,0 +1,75 @@
+from os import path
+from setuptools import setup, find_packages
+import sys
+import versioneer
+
+
+# NOTE: This file must remain Python 2 compatible for the foreseeable future,
+# to ensure that we error out properly for people with outdated setuptools
+# and/or pip.
+min_version = (3, 6)
+if sys.version_info < min_version:
+    error = """
+clarite does not support Python {0}.{1}.
+Python {2}.{3} and above is required. Check your Python version like so:
+
+python3 --version
+
+This may be due to an out-of-date pip. Make sure you have pip >= 9.0.1.
+Upgrade pip like so:
+
+pip install --upgrade pip
+""".format(
+        *(sys.version_info[:2] + min_version)
+    )
+    sys.exit(error)
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, "README.rst"), encoding="utf-8") as readme_file:
+    readme = readme_file.read()
+
+with open(path.join(here, "requirements/requirements.txt")) as requirements_file:
+    # Parse requirements.txt, ignoring any commented-out lines.
+    requirements = [
+        line
+        for line in requirements_file.read().splitlines()
+        if not line.startswith("#")
+    ]
+
+
+setup(
+    name="clarite",
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    description="CLeaning to Analysis: Reproducibility-based Interface for Traits and Exposures",
+    long_description=readme,
+    long_description_content_type="text/x-rst",
+    author="Hall Lab",
+    author_email="mah546@psu.edu",
+    url="https://github.com/HallLab/clarite-python",
+    python_requires=">={}".format(".".join(str(n) for n in min_version)),
+    packages=find_packages(exclude=["docs", "tests"]),
+    entry_points={
+        "console_scripts": [
+            "clarite-cli=clarite.cli:entry_point",
+        ],
+    },
+    include_package_data=True,
+    package_data={
+        "clarite": [
+            # When adding files here, remember to update MANIFEST.in as well,
+            # or else they will not be included in the distribution on PyPI!
+            # 'path/to/data_file',
+        ]
+    },
+    install_requires=requirements,
+    license="BSD-3-Clause",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python :: 3",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+    ],
+)
